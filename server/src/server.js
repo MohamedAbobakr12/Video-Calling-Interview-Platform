57c4758036965c75fba768ff13681a871277ea73
MohamedAbobakr12 <ziadabobakr@gmail.com>
2026-01-19 19:05:39 +0200

    deployment config added

--- server/src/server.js
+++ server/src/server.js
@@ -1,13 +1,28 @@
 import express from "express";
+import path from "path";
 import dotenv from "dotenv";
+
 import { ENV } from "./lib/env.js";
 
-dotenv.config();
-
 const app = express();
+
+const __dirname = path.resolve();
 
 app.get("/health", (req, res) => {
     res.status(200).json({ msg: "api is up and running" });
 })
+
+app.get("/health", (req, res) => {
+    res.status(200).json({ msg: "this is the books endpoint" });
+})
+
+// make our app ready for deployment
+if (ENV.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../client/dist")));
+
+    app.get("/{*any}", (req, res) => {
+        res.sendFile(path.join(__dirname, "../client", "dist", "index.html"));
+    });
+}
 
 app.listen(ENV.PORT, () => console.log("Server is running on http://localhost:", ENV.PORT));
